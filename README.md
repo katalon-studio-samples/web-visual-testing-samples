88e27dabae1aa2412de3c7d1db868e2cd13721ec
Giang Tong <giang.tong@katalon.com>
2020-11-26 16:17:32 +0700

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Visual Testing sample
+
+Firstly, To make sure this project run as expected, read [this guide](https://docs.katalon.com/katalon-studio/docs/katalon-analytics-beta-integration.html#settings) to enable TestOps Integration.
+
+For further information, visit [our forum post](https://forum.katalon.com/t/update-with-katalon-studio-7-7-early-release-of-katalon-testops-visual-testing-image-comparison/45557) for tutorials and Q&As.
---
+++ Test Suites/TS_RegressionTestCollection_With TestOps Vision.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteCollectionEntity>
+   <description></description>
+   <name>TS_RegressionTestCollection_With TestOps Vision</name>
+   <tag></tag>
+   <executionMode>SEQUENTIAL</executionMode>
+   <maxConcurrentInstances>1</maxConcurrentInstances>
+   <testSuiteRunConfigurations>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>default</profileName>
+            <runConfigurationId>Chrome</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/TS_RegressionTest_With TestOps Vision</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>default</profileName>
+            <runConfigurationId>Firefox</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/TS_RegressionTest_With TestOps Vision</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+   </testSuiteRunConfigurations>
+</TestSuiteCollectionEntity>
